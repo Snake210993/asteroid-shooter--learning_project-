32683af6622ef56e2ff1d7afc8edb50eafe366c3
Oliver <oliver@mhh.ch>
2025-10-20 22:25:07 +0200

    asteroid movement implemented

--- core/scenes/objects/script/player_ship.gd
+++ core/scenes/objects/script/player_ship.gd
@@ -3,12 +3,12 @@ extends Node2D
 @onready var ship: CharacterBody2D = $Ship
 @onready var health: Node2D = $Ship/Health
 
+@export var speed := 200
+@export var turn_speed := 3.0
 
-@export var speed := 500.0
-var rotation_offset := PI/2 # set to PI/2 if your texture points up
-var rotation_speed := 3.0 # rad/sec (~343°/s)
-
-var acceleration := 5.0
+const TURN_LEFT = -1.0
+const TURN_RIGHT = 1.0
+const BREAKING_POWER := 2.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,7 +19,7 @@ func _physics_process(delta: float) -> void:
 	
 	_movement_logik(delta)
 	if Input.is_action_just_pressed("Fire"): _fire_projectile()
-	
+
 func _fire_projectile() -> void:
 	var PROJECTILE = load("res://reusable/components/projectile.tscn")
 	var projectile = PROJECTILE.instantiate()
@@ -29,21 +29,25 @@ func _fire_projectile() -> void:
 	
 
 func _movement_logik(delta) -> void:
-
-	var direction := Input.get_vector("Left","Right","Up","Down")
-	var target_velocity := direction * speed
 	
-	_apply_rotation(direction, delta)
-
-	ship.velocity = ship.velocity.lerp(target_velocity, clamp(acceleration * delta, 0.0, 1.0))
-	ship.move_and_slide()	
+	var direction
+	if Input.is_action_pressed("Left"): 
+		direction = TURN_LEFT * turn_speed
+	elif Input.is_action_pressed("Right"):
+		direction =  TURN_RIGHT * turn_speed
+	else: direction = 0
 	
-func _apply_rotation(direction: Vector2, delta: float) -> void:
-	if direction != Vector2.ZERO:
-		var target_rotation = direction.angle() + rotation_offset
-		var start_rotation = ship.rotation
-		ship.rotation = lerp_angle(start_rotation, target_rotation, (rotation_speed  * delta))
-
+	ship.rotation += direction * delta
+	
+	
+	if Input.is_action_pressed("Up"):
+		var thrust_direction
+		thrust_direction = Vector2.UP.rotated(ship.rotation)
+		ship.velocity += thrust_direction * speed * delta
+	else:
+		ship.velocity = ship.velocity.lerp(Vector2.ZERO, clamp(BREAKING_POWER * delta, 0.0, 1.0))
+	
+	ship.move_and_slide() ## move and slide needs to be after any changed are made to velocity
 
 func _on_health_zero_health_reached() -> void:
 	queue_free()
